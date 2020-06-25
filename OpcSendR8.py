96d802c3a4f20deb1e1e50cb4906266ac34a808a
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2020-06-26 02:10:20 +0300

    auth + thread

--- OpcSendR8.py
+++ OpcSendR8.py
@@ -3,8 +3,9 @@ import OpenOPC
 import time
 import requests
 import telebot
+import threading
 
-token = ""
+token = "1203397890:AAF3Z53lbtmCWkXlsxJl4fXRj6Dtcv6TEc0"
 ###################################################
 #
 def run_setting():
@@ -49,7 +50,7 @@ while True:
 def send_telegram(text: str):
     token = "1203397890:AAFupM7Z1QXBuTOdDI2lwgpPwmYYAgX0p4o"
     url = "https://api.telegram.org/bot"
-    channel_id = ""
+    channel_id = "@R8Plant"
     url += token
     method = url + "/sendMessage"
 
@@ -84,73 +85,87 @@ def append_val(tagsValue):
 
 ###################################################
 
+def send_mess():
+    while True:
+        print("-----------------------------------------------------------------------------------------")
+        try:
+            ############################################################
+            # Проверка температур слоя реактора
+            val = opc.read(tagsValue, update=1, include_error=True)
+            if int(val[0][1]) > (react_temp + r_tmp_lim) or int(val[1][1]) > (react_temp + r_tmp_lim) or int(
+                    val[2][1]) > (react_temp + r_tmp_lim):
+                try:
+                    # print("here")
+                    send_telegram(
+                        "Слой реактора перегрет до {0:.2f} {1:.2f} {2:.2f} С".format(val[0][1], val[1][1], val[2][1]))
+                except:
+                    print("Включите VPN (test 1)")
+            if int(val[0][1]) < (react_temp - r_tmp_lim) or int(val[1][1]) < (react_temp - r_tmp_lim) or int(
+                    val[2][1]) < (react_temp - r_tmp_lim):
+                try:
+                    send_telegram(
+                        "Слой реактора охладился до {0:.2f} {1:.2f} {2:.2f} С".format(val[0][1], val[1][1], val[2][1]))
+                except:
+                    print("Включите VPN (test 2)")
+            ############################################################
+            # Проверка температуры слоя форреактора
+            if int(val[3][1]) > (for_react_temp + fr_tmp_lim):
+                try:
+                    send_telegram("Слой ФР перегрет до {0:.2f} С".format(val[3][1]))
+                except:
+                    print("Включите VPN (test 3)")
+            if int(val[3][1]) < (for_react_temp - fr_tmp_lim):
+                try:
+                    send_telegram("Слой ФР охладился до {0:.2f} С".format(val[3][1]))
+                except:
+                    print("Включите VPN (test 4)")
+            ############################################################
+            # Проверка давления СВД
+            if (val[4][1]) > (prs + prs_lim):
+                try:
+                    send_telegram("Давление поднялось до {0:.2f} ати".format(val[4][1]))
+                except:
+                    print("Включите VPN (test 5)")
+            if (val[4][1]) < (prs - prs_lim):
+                try:
+                    send_telegram("Давление опустилось до {0:.2f} ати".format(val[4][1]))
+                except:
+                    print("Включите VPN (test 6)")
+            i = 0
+            for el in tagsValue:
+                print("{} {}".format(el, val[i][1]))
+                i += 1
+
+        except:
+            print("error read item")
+        time.sleep(poll_time)
+
+###################################################
 opc = OpenOPC.client()
 servers = opc.servers()
-#print(servers)
+i = 0
+print("-----------------------------------------------------------------------------------------")
+for serv in servers:
+    print(str(i) + " " + serv)
+    i += 1
+print("-----------------------------------------------------------------------------------------")
+num_serv = int(input("Введите номер сервера к которому нужно подключиться: "))
 try:
-    opc.connect("Owen.OPCNet.DA.1")
+    opc.connect(servers[num_serv])
     print("-----------------------------------------------------------------------------------------")
-    print("Удачное подключение к " + servers[0])
+    print("Удачное подключение к " + servers[num_serv])
 except:
-    print("не удалось подключиться к ОРС - серверу")
-#list_par = opc.list('COM4.*.Оперативные параметры')
-
-#for elem in list_par:
-#    print(elem)
+    print("не удалось подключиться к " + servers[num_serv])
 #--------------------------------------------------------------------------------------------------------#
 tagsValue = [];
 append_val(tagsValue)
-#print(opc.list('COM4.TRM_202(adr=112)T_слой_Пр_р-ра'))
 
-while True:
-    print("-----------------------------------------------------------------------------------------")
-    try:
-        ############################################################
-        # Проверка температур слоя реактора
-        val = opc.read(tagsValue, update=1, include_error=True)
-        if int(val[0][1]) > (react_temp + r_tmp_lim) or int(val[1][1]) > (react_temp + r_tmp_lim) or int(val[2][1]) > (react_temp + r_tmp_lim):
-            try:
-                #print("here")
-                send_telegram("Слой реактора перегрет до {0:.2f} {1:.2f} {2:.2f} С".format(val[0][1], val[1][1], val[2][1]))
-            except:
-                print("Включите VPN (test 1)")
-        if int(val[0][1]) < (react_temp - r_tmp_lim) or int(val[1][1]) < (react_temp - r_tmp_lim) or int(val[2][1]) < (react_temp - r_tmp_lim):
-            try:
-                send_telegram("Слой реактора охладился до {0:.2f} {1:.2f} {2:.2f} С".format(val[0][1], val[1][1], val[2][1]))
-            except:
-                print("Включите VPN (test 2)")
-        ############################################################
-        # Проверка температуры слоя форреактора
-        if int(val[3][1]) > (for_react_temp + fr_tmp_lim):
-            try:
-                send_telegram("Слой ФР перегрет до {0:.2f} С".format(val[3][1]))
-            except:
-                print("Включите VPN (test 3)")
-        if int(val[3][1]) < (for_react_temp - fr_tmp_lim):
-            try:
-                send_telegram("Слой ФР охладился до {0:.2f} С".format(val[3][1]))
-            except:
-                print("Включите VPN (test 4)")
-        ############################################################
-        # Проверка давления СВД
-        if (val[4][1]) > (prs + prs_lim):
-            try:
-                send_telegram("Давление поднялось до {0:.2f} ати".format(val[4][1]))
-            except:
-                print("Включите VPN (test 5)")
-        if (val[4][1]) < (prs - prs_lim):
-            try:
-                send_telegram("Давление опустилось до {0:.2f} ати".format(val[4][1]))
-            except:
-                print("Включите VPN (test 6)")
-        i = 0
-        for el in tagsValue:
-            print("{} {}".format(el, val[i][1]))
-            i += 1
+send_mess()
+#thrd_send_mess = threading.Thread(target=send_mess)
+#thrd_send_mess.start()
 
-    except:
-        print("error read item")
-    time.sleep(poll_time)
+#thrd_run_setting = threading.Thread(target=run_setting)
+#thrd_run_setting.start()
 
-opc.close()
+#opc.close()
 #---------------------------#
